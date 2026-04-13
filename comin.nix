757cf541506f6a72d2d2837b52de420e313dfab7
maurice <maurice@wsl.localdomain>
2026-04-13 22:12:04 +0000

    updated git repo

--- comin.nix
+++ comin.nix
@@ -5,7 +5,7 @@
     enable = true;
     remotes = [{
       name = "origin";
-      url = "git@github.com:your-username/your-repo.git";
+      url = "git@github.com:Sapp00/nix-infra.git";
       auth.access_token_path = "/etc/ssh/github_deploy_key"; # The baked-in key
     }];
   };
