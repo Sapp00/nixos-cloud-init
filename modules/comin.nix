3fea8795ec57ca212ecf073b99ae55e0c2b2ced0
maurice <maurice@wsl.localdomain>
2026-04-16 12:56:28 +0000

    updated comin config

--- modules/comin.nix
+++ modules/comin.nix
@@ -6,6 +6,7 @@
     remotes = [{
       name = "origin";
       url = "https://github.com/Sapp00/nix-infra.git";
+      branches.main.name = "main";
       auth.access_token_path = config.sops.secrets."github/access_token".path;
     }];
   };
